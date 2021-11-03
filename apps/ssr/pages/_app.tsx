fe3deb2ef58ddafa6fa97f6650222debca1103c5
vpvnguyen <vpvnguyen@gmail.com>
2021-11-03 09:13:16 -0700

    initial scaffold of nx builder; basic routing

--- apps/ssr/pages/_app.tsx
+++ apps/ssr/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import Header from '../components/Header';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,11 +10,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <title>Welcome to ssr!</title>
       </Head>
       <div className="app">
-        <header className="flex">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/nx-logo-white.svg" alt="Nx logo" width="75" height="50" />
-          <h1>Welcome to ssr!</h1>
-        </header>
+        <Header />
         <main>
           <Component {...pageProps} />
         </main>
